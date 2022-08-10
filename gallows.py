db97c9755d01a52ab9815591e5b214b9a148c5d9
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2022-08-10 12:07:12 +0300

    Update gallows.py
    gallows.py v3

--- gallows.py
+++ gallows.py
@@ -1,13 +1,13 @@
-rom pathlib import Path
-root_dir_path = Path.cwd()
-## Get file's absolute path
-abs_path = Path('gallows.py').absolute()
-print(f'{abs_path = }')
-## create a data folder in the directory with the gallows.py file
-Path("data").mkdir(exist_ok = True)
-## create a text.txt file and write a line to it
-d = Path(root_dir_path, 'data/text.txt')
-d.write_text('Hi, im a text in file!')
-## print the contents of the file to the screen
-consinstance_of_file = d.read_text()
-print(f'{consinstance_of_file = }')
+from pathlib import Path
+root_dir_path = Path()
+# get file's absolute path
+file_abs_path = (root_dir_path / 'gallows.py').absolute()
+print(f'{file_abs_path = }')
+# create a data folder in the directory with the gallows.py file
+(root_dir_path / 'data').mkdir(exist_ok=True)
+# create a text.txt file and write a line to it
+file = root_dir_path / 'data/text.txt'
+file.write_text('Hi, im a text in file!')
+# print the contents of the file to the screen
+file_content = file.read_text()
+print(f'{file_content = }')
