b2c289d2e8e837c8428753a738f6992bf7fe7cc1
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2022-08-10 11:16:05 +0300

    update gallows.py v2
    the specified shortcomings are corrected

--- gallows.py
+++ gallows.py
@@ -1,12 +1,13 @@
-from pathlib import Path
-## получаем абсолютный путь файла
-pat = Path(Path.cwd(), Path("gallows.py"))
-print(f'Абсолютный путь файла - {pat}')
-## создаем папку data, в директории с файлом gallows.py
-papka = Path("data").mkdir()
-## создаем файл text.txt и записываем в него строку
-d = Path(Path.cwd(), Path("data/text.txt"))
-d.touch()
+rom pathlib import Path
+root_dir_path = Path.cwd()
+## Get file's absolute path
+abs_path = Path('gallows.py').absolute()
+print(f'{abs_path = }')
+## create a data folder in the directory with the gallows.py file
+Path("data").mkdir(exist_ok = True)
+## create a text.txt file and write a line to it
+d = Path(root_dir_path, 'data/text.txt')
 d.write_text('Hi, im a text in file!')
-## выводим содержимое строки на экран
-print(d.read_text())
+## print the contents of the file to the screen
+consinstance_of_file = d.read_text()
+print(f'{consinstance_of_file = }')
