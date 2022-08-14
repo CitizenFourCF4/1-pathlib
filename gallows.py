4d5907290d20eae27cd0af26a87dad5de06b19c5
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2022-08-14 13:18:28 +0300

    Update gallows.py v4

--- gallows.py
+++ gallows.py
@@ -1,13 +1,18 @@
 from pathlib import Path
+
 root_dir_path = Path()
+
 # get file's absolute path
 file_abs_path = (root_dir_path / 'gallows.py').absolute()
 print(f'{file_abs_path = }')
+
 # create a data folder in the directory with the gallows.py file
 (root_dir_path / 'data').mkdir(exist_ok=True)
+
 # create a text.txt file and write a line to it
-file = root_dir_path / 'data/text.txt'
-file.write_text('Hi, im a text in file!')
+output_file_path = root_dir_path / 'data' / 'text.txt'
+output_file_path.write_text('Hi, im a text in file!')
+
 # print the contents of the file to the screen
-file_content = file.read_text()
+file_content = output_file_path.read_text()
 print(f'{file_content = }')
