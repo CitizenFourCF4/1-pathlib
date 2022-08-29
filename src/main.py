510f4dfa29b27e7f24bd08f3be925490b3e414ae
CitizenFourCF4 <94146075+CitizenFourCF4@users.noreply.github.com>
2022-08-29 22:22:18 +0300

    Update main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-from functions import parsing_of_command_line
+from functions import parse_command_line_args
+from functions import set_of_logger
+from functions import check_for_user_input
 
 
 def main():
+    set_of_logger()
+
     input_text, output_file_path = parse_command_line_args()
 
+    check_for_user_input()
     # creating folders if the current directory does not exist
     output_file_path.parent.mkdir(exist_ok=True, parents=True)
     # create a file and write a text to it
